a7875bd49a13fb31e46a46875d7c2ae096dd0772
Polina Shlepakova <polinashlepakova@gmail.com>
2019-11-21 15:45:20 +0200

    Add Authorization DTO with password encryption
    Signed-off-by: Polina Shlepakova <polinashlepakova@gmail.com>

---
+++ Models/AuthorizationDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Notes.Tools;
+
+namespace Notes.Models
+{
+    [Serializable]
+    class AuthorizationDTO
+    {
+        #region Properties
+        public string Login { get; private set; }
+        private string Password { get; set; }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="login">User's login.</param>
+        /// <param name="password">Clear text password, which will be hashed using login as salt.</param>
+        public AuthorizationDTO(string login, string password)
+        {
+            Login = login;
+            SetPassword(password);
+        }
+        #endregion
+
+        /// <summary>
+        /// Sets password after hashing it with salt (login).
+        /// </summary>
+        /// <param name="password">Clear text password.</param>
+        private void SetPassword(string password)
+        {
+            Password = EncryptionHelper.GenerateHash(password, Login);
+        }
+    }
+}
---
+++ Tools/EncryptionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Notes.Tools
+{
+    internal static class EncryptionHelper
+    {
+        /// <summary>
+        /// Generate hash for input.
+        /// </summary>
+        /// <param name="input">String to hash.</param>
+        /// <param name="salt">Salt to prepend to the input before hashing.</param>
+        internal static string GenerateHash(string input, string salt)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(salt + input);
+            SHA256Managed sha256ManagedString = new SHA256Managed();
+            byte[] hash = sha256ManagedString.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+    }
+}

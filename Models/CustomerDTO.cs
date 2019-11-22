8e7f5888431f2385538d0a7ca5b19d0a5fbda2fb
Polina Shlepakova <polinashlepakova@gmail.com>
2019-11-22 10:12:21 +0200

    Refactor User DTOs
    Signed-off-by: Polina Shlepakova <polinashlepakova@gmail.com>

--- Models/CustomerDTO.cs
+++ Models/CustomerDTO.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using Notes.Tools;
+﻿using Notes.Tools;
 using System.Runtime.Serialization;
 
 namespace Notes.Models
 {
     [DataContract]
-    internal class AuthorizationDTO
+    internal class CustomerDTO
     {
         #region Fields
         [DataMember(Name = "Login")]
         private string _login;
         [DataMember(Name = "Password")]
         private string _password;
+        [DataMember(Name = "FirstName")]
+        private string _firstName;
+        [DataMember(Name = "LastName")]
+        private string _lastName;
+        [DataMember(Name = "Email")]
+        private string _email;
         #endregion
 
         #region Properties
@@ -26,16 +31,40 @@ namespace Notes.Models
             get => _password;
             set => _password = value;
         }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
         #endregion
 
         #region Constructor
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
+        /// <param name="firstName">User's first name.</param>
+        /// <param name="lastName">User's last name.</param>
+        /// <param name="email">User's email.</param>
         /// <param name="login">User's login.</param>
         /// <param name="password">Clear text password, which will be hashed using login as salt.</param>
-        public AuthorizationDTO(string login, string password)
+        public CustomerDTO(string firstName, string lastName, string email, string login, string password)
         {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
             Login = login;
             SetPassword(password);
         }
